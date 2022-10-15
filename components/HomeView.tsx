bb127a06de092a2404d72a93615c1c82d969380a
Artur Padovesi Piratelli <apiravesi@gmail.com>
2022-10-15 20:12:46 -0300

    styles changed

--- components/HomeView.tsx
+++ components/HomeView.tsx
@@ -13,7 +13,7 @@ export default function HomeView({
     activity, handleClick, nextActivityIsLoading
 }: props
 ) {
-    return <div className="flex flex-col p-10">
+    return <div className="flex flex-col p-10 h-full">
       <Head>
         <title>Bored? Not anymore</title>
         <meta name="description" content="A bored api view for usability" />
@@ -22,26 +22,27 @@ export default function HomeView({
 
       <Navbar />
 
-      <main className="flex flex-col">
+      <main className="flex flex-col flex-grow">
         <h1 className="text-3xl font-bold m-4">
           Bored? Hopefully not anymore.
         </h1>
         <h2 className="text-2xl m-8">
           What should you do?
         </h2>
-        <span className="self-center text-xl border-2 bg-zinc-800 border-zinc-900 w-2/3 flex justify-center p-10">
+        <span className="my-auto self-center text-xl border-2 bg-zinc-800 border-zinc-900 w-2/3 flex justify-center p-10">
           {activity.activity}
         </span>
       </main>
       <button onClick={handleClick}
         disabled={nextActivityIsLoading}
-        className={`bg-slate-600 p-3 rounded mx-auto my-7 ${nextActivityIsLoading && "bg-slate-800 cursor-default"}`}
+        className={`bg-slate-600 p-3 rounded mx-auto
+        my-20 hover:shadow-slate-300 hover:shadow-2xl ${nextActivityIsLoading && "bg-slate-800 cursor-default"}`}
       >
         Another one
       </button>
       {
         nextActivityIsLoading &&
-        <span className="mx-auto">
+        <span className="mx-auto absolute top-1/3 left-1/2 -translate-x-1/2">
           Loading...
           </span>
       }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,12 +4,18 @@ export default function Navbar()  {
   return (
     <nav>
         <ul>
-            <li>
+            <li
+              className="bg-neutral-900 rounded opacity-90 hover:opacity-100
+               hover:bg-zinc-900 hover:shadow-slate-700 hover:shadow-sm"
+            >
               <Link href="/">
                 Without prefetch
               </Link>
             </li>
-            <li>
+            <li
+              className="bg-neutral-900 rounded opacity-90 hover:opacity-100
+               hover:bg-zinc-900 hover:shadow-slate-700 hover:shadow-sm"
+            >
               <Link href="/with_prefetch">
                 With prefetch
               </Link>
